f2af47c3b4dcf7f77050490b96c9f14ced8596de
Thiago Mathias <thiago.santos@modalgr.com.br>
2022-05-04 01:00:22 -0300

    fix

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import Apresentacao from './src/screens/Apresentacao/Apresentacao';
+import Login from './src/screens/Login/Login';
+
+export default function App() {
+  return <Apresentacao />
+}
