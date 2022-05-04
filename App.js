6ade55556b62febeb4e9a2034d03f6a96614b6e5
Thiago Mathias <thiago.santos@modalgr.com.br>
2022-05-04 00:48:12 -0300

    remove useless files

--- App.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import Apresentacao from './src/screens/Apresentacao/Apresentacao';
-import Login from './src/screens/Login/Login';
-
-export default function App() {
-  return <Apresentacao />
-}
